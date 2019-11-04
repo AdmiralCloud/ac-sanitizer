b87b96503acb880a91ae73536a2fb52a4c84c4f8
Mark Poepping <mark.poepping@mmpro.de>
2019-11-04 19:29:17 +0100

    fix(AC Sanitizer): Add test for emails like xy+123@abc.de | MP
    Add test for emails like xy+123@abc.de

--- test/tests/email.js
+++ test/tests/email.js
@@ -9,7 +9,8 @@ module.exports = {
     const baseTests = [
       { name: 'Valid email', type: 'email', value: 'jane.doe@admiralcloud.com', expected: 'jane.doe@admiralcloud.com' },
       { name: 'Invalid email', type: 'email', value: 'jane.doe@admiralcloud', error: 'email_notAValidEmailAddress' },
-      { name: 'Invalid email', type: 'email', value: 'jane.doe', error: 'email_notAValidEmailAddress' }
+      { name: 'Invalid email', type: 'email', value: 'jane.doe', error: 'email_notAValidEmailAddress' },
+      { name: 'Valid email', type: 'email', value: 'jane.doe+mediahub002@admiralcloud.com', expected: 'jane.doe+mediahub002@admiralcloud.com' },
     ]
 
 
