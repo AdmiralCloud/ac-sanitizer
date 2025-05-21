0d335b956b012023bbe6f3214d2d961ff8baa04a
Mark Poepping <mark.poepping@admiralcloud.com>
2025-05-21 17:56:45 +0200

    [master]  test(App): Fixed test | MP
    Fixed test
    Related issues:
    - undefined/browse/master

--- test/tests/url.js
+++ test/tests/url.js
@@ -7,7 +7,7 @@ module.exports = {
     const baseTests = [
       { name: 'Valid url', type: 'url', value: 'https://www.admiralcloud.com', expected: 'https://www.admiralcloud.com' },
       { name: 'Invalid url', type: 'url', value: 'www.admiralcloud.com', error: 'url_notAValidURL' },
-      { name: 'Invalid url - wrong protocol', type: 'url', value: 'ftp://www.admiralcloud.com', error: 'url_notAValidURL' },
+      { name: 'Invalid url - wrong protocol', type: 'url', value: 'ftp://www.admiralcloud.com', error: 'url_notAValidURL' }
     ]
 
     runValidationTests(baseTests, 'url')
